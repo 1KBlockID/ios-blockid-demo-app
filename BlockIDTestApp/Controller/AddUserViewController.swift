0b4ce8fec21901e960a7bd84ed8d3dc0d12b919f
kuldeep-1k <kuldeep@1kosmos.com>
2022-10-12 00:52:03 +0530

    - updated SDK with BIDVCEmployee object added - store vc_employee data with user data - add VC to my cards if vc_employee present

--- BlockIDTestApp/Controller/AddUserViewController.swift
+++ BlockIDTestApp/Controller/AddUserViewController.swift
@@ -366,6 +366,31 @@ extension AddUserViewController {
 //                    })
 //                }
                 
+                if let user = user, let vcEmployeeObj = user.vc_employee {
+                    //
+                    // IF vc_employee EXISTS
+                    var vcEmployeeDic = [String : Any]()
+                    let strJson = CommonFunctions.objectToJSONString(vcEmployeeObj)
+                    if let vcDic = CommonFunctions.jsonStringToDic(from: strJson) {
+                        vcEmployeeDic = vcDic
+                    }
+                    
+                    if var cards = UserDefaults.standard.value(forKey: "VFC_CARDS") as? [[String: Any]] {
+                        //
+                        // IF_ALREADY VC CARDS EXISTS
+                        cards.append(["docType": CardType.employee_card.rawValue,
+                                                     "vfc": vcEmployeeDic])
+                        UserDefaults.standard.set(cards,
+                                                  forKey: "VFC_CARDS")
+
+                    } else {
+                        UserDefaults.standard.set([["docType": CardType.employee_card.rawValue,
+                                                   "vfc": vcEmployeeDic]],
+                                                  forKey: "VFC_CARDS")
+                    }
+                    
+                }
+                
                 self.view.makeToast("User registration successful.",
                                     duration: 3.0,
                                     position: .center,
