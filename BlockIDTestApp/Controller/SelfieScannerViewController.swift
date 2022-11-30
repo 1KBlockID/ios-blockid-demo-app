996ff9f457c5c4054c0d6dc15e5aacdf39d3603b
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2022-11-30 22:12:58 +0530

    MOB-3672-ios-implement-live-id-selfie-scanning

--- BlockIDTestApp/Controller/SelfieScannerViewController.swift
+++ BlockIDTestApp/Controller/SelfieScannerViewController.swift
@@ -37,7 +37,7 @@ extension SelfieScannerViewController {
         }
         self.navigationController?.popViewController(animated: true)
     }
-
+    
     private func startLiveIDScanning() {
         // Check for Camera Permission
         AVCaptureDevice.requestAccess(for: AVMediaType.video) { response in
@@ -48,25 +48,25 @@ extension SelfieScannerViewController {
                 }
             } else {
                 DispatchQueue.main.async {
-                        SelfieScannerHelper.shared.startLiveIDScan(from: self) { status, data, _ in
-                            if status {
-                                guard let liveidDataDic = data else { return }
-                                if self.isForVerification {
-                                    guard let imgdataB64 = liveidDataDic["liveId"] as? String else { return }
-                                    guard let imgdata = Data(base64Encoded: imgdataB64,
-                                                             options: .ignoreUnknownCharacters),
-                                          let photo = UIImage(data: imgdata) else { return }
-                                    // Verify LiveID
-                                    self.verifyLiveID(withPhoto: photo,
-                                                      token: nil)
-                                } else {
-                                    // Set LiveID
-                                    self.checkLiveness(liveidImgDic: liveidDataDic)
-                                }
+                    SelfieScannerHelper.shared.startLiveIDScan(from: self) { status, data, _ in
+                        if status {
+                            guard let liveidDataDic = data else { return }
+                            if self.isForVerification {
+                                guard let imgdataB64 = liveidDataDic["liveId"] as? String else { return }
+                                guard let imgdata = Data(base64Encoded: imgdataB64,
+                                                         options: .ignoreUnknownCharacters),
+                                      let photo = UIImage(data: imgdata) else { return }
+                                // Verify LiveID
+                                self.verifyLiveID(withPhoto: photo,
+                                                  token: nil)
                             } else {
-                                self.goBack()
+                                // Set LiveID
+                                self.checkLiveness(liveidImgDic: liveidDataDic)
                             }
+                        } else {
+                            self.goBack()
                         }
+                    }
                 }
             }
         }
@@ -111,7 +111,7 @@ extension SelfieScannerViewController {
         var liveidDataDic = liveidImgDic
         liveidDataDic["id"] = BlockIDSDK.sharedInstance.getDID() + ".liveid"
         liveidDataDic["type"] = "liveid"
-
+        
         BlockIDSDK.sharedInstance.verifyDocument(dic: liveidDataDic,
                                                  verifications: ["face_liveness"])
         { [weak self] (status, dataDic, error) in
@@ -120,25 +120,25 @@ extension SelfieScannerViewController {
                 DispatchQueue.main.async {
                     self?.view.hideToastActivity()
                     self?.view.makeToast(error?.message,
-                                        duration: 3.0,
-                                        position: .center,
-                                        title: "Error!",
-                                        completion: {_ in
+                                         duration: 3.0,
+                                         position: .center,
+                                         title: "Error!",
+                                         completion: {_ in
                         self?.goBack()
                     })
                     return
                 }
             } else {
                 if let dataDict = dataDic,
-                    let certifications = dataDict["certifications"] as? [[String: Any]] {
+                   let certifications = dataDict["certifications"] as? [[String: Any]] {
                     if let isVerified = certifications[0]["verified"] as? Bool, isVerified {
-
+                        
                         guard let imgdataB64 = liveidDataDic["liveId"] as? String else { return }
-
+                        
                         guard let imgdata = Data(base64Encoded: imgdataB64,
                                                  options: .ignoreUnknownCharacters),
                               let img = UIImage(data: imgdata) else { return }
-
+                        
                         self?.setLiveID(withPhoto: img, token: nil)
                     } else {
                         let errorVerificationFailed = ErrorResponse(code: CustomErrors.kFaceLivenessCheckFailed.code,
@@ -147,10 +147,10 @@ extension SelfieScannerViewController {
                         DispatchQueue.main.async {
                             self?.view.hideToastActivity()
                             self?.view.makeToast(errorVerificationFailed.message,
-                                                duration: 3.0,
-                                                position: .center,
-                                                title: "Error!",
-                                                completion: {_ in
+                                                 duration: 3.0,
+                                                 position: .center,
+                                                 title: "Error!",
+                                                 completion: {_ in
                                 self?.goBack()
                             })
                             return
@@ -197,15 +197,13 @@ extension SelfieScannerViewController {
     private func showErrorDialog(_ error: ErrorResponse?) {
         var title: String? = nil
         var msg: String? = nil
-        if error?.code == NSURLErrorNotConnectedToInternet ||
+        if (error != nil && error?.code == CustomErrors.kUnauthorizedAccess.code) {
+            self.showAppLogin()
+        } else if error?.code == NSURLErrorNotConnectedToInternet ||
             error?.code == CustomErrors.Network.OFFLINE.code {
             msg = "OFFLINE".localizedMessage(CustomErrors.Network.OFFLINE.code)
             title = ErrorConfig.noInternet.title
-        }
-        else if (error != nil && error?.code == CustomErrors.kUnauthorizedAccess.code) {
-            self.showAppLogin()
-        }
-        else {
+        } else else {
             msg = error!.message
         }
         self.view.makeToast(msg, duration: 3.0,
