a112697bb20988faaa9ad0b099bcc19b8f7a722b
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2023-01-04 18:18:10 +0530

    MOB-3757-i-os-provide-interface-for-auid-document-scanner-sdk

--- BlockIDTestApp/Controller/DriverLicenseViewController.swift
+++ BlockIDTestApp/Controller/DriverLicenseViewController.swift
@@ -90,30 +90,29 @@ class DriverLicenseViewController: UIViewController {
                 }
             } else {
                 DispatchQueue.main.async {
-                    self._viewBG.isHidden = true
-                    self._viewLiveIDScan.isHidden = true
-                    self.activityIndicator.startAnimating()
                     //3. Initialize dlScannerHelper
                     if self.dlScannerHelper == nil {
                         // NOTE: Uncomment below code to scan document with BIDScannerView
-                    /*
-                     self._scanLine = self.addScanLine(self._imgOverlay.frame)
-                     self._imgOverlay.layer.addSublayer(self._scanLine)
-                     self._lblScanInfoTxt.text = DLScanningSide.DL_BACK == self.firstScanningDocSide ? "Scan Back" : "Scan Front"
-
-                     self.dlScannerHelper = DriverLicenseScanHelper.init(scanningMode: self.selectedMode,
-                    bidScannerView: self._viewLiveIDScan,
-                    dlScanResponseDelegate: self,
-                    cutoutView: self._imgOverlay.frame,
-                    expiryGracePeriod: self.expiryDays)*/
+                       /* self._viewBG.isHidden = false
+                        self._viewLiveIDScan.isHidden = false
+                        self._scanLine = self.addScanLine(self._imgOverlay.frame)
+                        self._imgOverlay.layer.addSublayer(self._scanLine)
+                        self._lblScanInfoTxt.text = DLScanningSide.DL_BACK == self.firstScanningDocSide ? "Scan Back" : "Scan Front"
                         
-                        
+                        self.dlScannerHelper = DriverLicenseScanHelper.init(scanningMode: self.selectedMode,
+                                                                            bidScannerView: self._viewLiveIDScan,
+                                                                            dlScanResponseDelegate: self,
+                                                                            cutoutView: self._imgOverlay.frame,
+                                                                            expiryGracePeriod: self.expiryDays)*/
+                        self.activityIndicator.startAnimating()
+                        self._viewBG.isHidden = true
+                        self._viewLiveIDScan.isHidden = true
                         self.dlScannerHelper = DriverLicenseScanHelper.init(dlScanResponseDelegate: self)
                     }
                     //4. Start Scanning
                     self.dlScannerHelper?.startDLScanning(scanningSide: self.firstScanningDocSide)
-
-                   
+                    
+                    
                 }
             }
         }
