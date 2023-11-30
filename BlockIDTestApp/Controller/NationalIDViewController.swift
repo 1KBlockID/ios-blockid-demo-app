0061441207d846df96aa2b3814d09a37a1932cc4
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2023-11-30 20:02:56 +0530

    MOB-5151-ios-demo-app-implementation-poll-document-session-and-return-metadata-to-app

--- BlockIDTestApp/Controller/NationalIDViewController.swift
+++ BlockIDTestApp/Controller/NationalIDViewController.swift
@@ -45,20 +45,6 @@ class NationalIDViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
-    @IBAction func cancelClicked(_ sender: Any) {
-        let alert = UIAlertController(title: "Cancellation warning!",
-                                      message: "Do you want to cancel the registration process?",
-                                      preferredStyle: .alert)
-
-        alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
-        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {_ in
-            self.nidScannerHelper?.stopNationalIDScanning()
-            self.goBack()
-        }))
-        self.present(alert, animated: true)
-        return
-    }
-    
     private func startNationalIDScanning() {
         //1. Check for Camera Permission
         AVCaptureDevice.requestAccess(for: AVMediaType.video) { response in
@@ -70,18 +56,6 @@ class NationalIDViewController: UIViewController {
             } else {
                 DispatchQueue.main.async {
                     self.showDocumentScannerFor(.IDCARD, self)
-                    
-                   /* self._viewBG.isHidden = false
-                    self._viewLiveIDScan.isHidden = false
-                    //3. Initialize dlScannerHelper
-                    if self.nidScannerHelper == nil {
-                        self.nidScannerHelper = NationalIDScanHelper.init(bidScannerView: self._viewLiveIDScan, nidScanResponseDelegate: self, cutoutView:  self._imgOverlay.frame, expiryGracePeriod: self.expiryDays)
-                    }
-                    //4. Start Scanning
-                    self._lblScanInfoTxt.text = NIDScanningSide.NATIONAL_ID_BACK == self.firstScanningDocSide ? "Scan Back" : "Scan Front"
-                    self.nidScannerHelper?.startNationalIDScanning(scanningSide: self.firstScanningDocSide)
-                    self._scanLine = self.addScanLine(self._imgOverlay.frame)
-                    self._imgOverlay.layer.addSublayer(self._scanLine)*/
                 }
             }
         }
@@ -94,15 +68,12 @@ class NationalIDViewController: UIViewController {
         }
         registrationCalled = true
         debugPrint("***** setNationaID")
-        //self._viewBG.isHidden = true
-//        self.view.makeToastActivity(.center)
         var dic = nid
         dic["category"] = RegisterDocCategory.Identity_Document.rawValue
         dic["type"] = RegisterDocType.NATIONAL_ID.rawValue
         dic["id"] = nid["id"] as! String
         BlockIDSDK.sharedInstance.registerDocument(obj: dic, sigToken: token) { [self] (status, error) in
             DispatchQueue.main.async {
-//                self.view.hideToastActivity()
                 if !status {
                     // FAILED
                     if error?.code == CustomErrors.kLiveIDMandatory.code {
@@ -158,50 +129,34 @@ extension NationalIDViewController: DocumentScanDelegate {
             return
         }
         
-        debugPrint("***** onDocumentScanResponse", document?.count, error?.message)
         if error?.code == CustomErrors.DocumentScanner.CANCELED.code { // Cancelled
             self.goBack()
         }
         
-        if let idCard = document {
-            guard let idCardObject = CommonFunctions.jsonStringToDic(from: idCard) else {
-                return
-            }
-            debugPrint("*****", idCardObject.keys)
-            
-            if var dictIdcardObject = idCardObject["idcard_object"] as? [String: Any] {
-                dictIdcardObject["token"] = idCardObject["token"]
-                debugPrint("*****", dictIdcardObject.keys)
-                
-                self.setNationaID(withNIDData: dictIdcardObject, token: "")
-            }
+        /*If responseStatus == SUCCESS *
+         AND token is NON-NULL/NON-EMPTY *
+         AND there is a dl_object OR a ppt_object OR a idcard_object *
+         AND the document has a proof_jwt
+         */
+        
+        guard let idCard = document else {
+           return
         }
-        
-        
-        //            scanCompleteUIUpdates()
-        //            guard let nid = dictNationalID, let token = signToken else {
-        //                self.view.makeToast(error?.message,
-        //                                    duration: 3.0,
-        //                                    position: .center)
-        //                return
-        //
-        //            }
-        
-        //        //Check if Not to Expiring Soon
-        //        if error?.code != CustomErrors.kDocumentAboutToExpire.code {
-        //            self.setNationaID(withNIDData: nid, token: token)
-        //            return
-        //        }
-        //            //About to Expire, Show Alert
-        //            let alert = UIAlertController(title: "Error", message: error!.message, preferredStyle: .alert)
-        //
-        //            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in
-        //                self.setNationaID(withNIDData: nid, token: token)
-        //            }))
-        //            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
-        //
-        //            self.present(alert, animated: true)
-        
+        guard let idCardObject = CommonFunctions.jsonStringToDic(from: idCard) else {
+            // Document data does not exist
+            return
+        }
+        guard let  responseStatus = idCardObject["responseStatus"] as? String, responseStatus == "SUCCESS" else {
+           return
+        }
+        guard var dictIdcardObject = idCardObject["idcard_object"] as? [String: Any] else {
+           return
+        }
+        guard let token = idCardObject["token"] else {
+            return
+        }
+        dictIdcardObject["token"] = token // Add proof_jwt to this key
+        self.setNationaID(withNIDData: dictIdcardObject, token: "")
     }
     
     private func showAlert(title: String, message: String) {
