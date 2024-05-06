124a8dee4ab4cc700757f2f14b22696324472307
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2024-05-06 20:58:37 +0530

    MOB-5873-block-id-demo-ios-report-an-event-for-reset-sdk

--- BlockIDTestApp/SupportingFiles/Extension/ViewControllerExtension.swift
+++ BlockIDTestApp/SupportingFiles/Extension/ViewControllerExtension.swift
@@ -57,7 +57,7 @@ extension UIViewController {
         //If launched using Magic-Link
         //Need to resave magic-link once app is reset
         UserDefaults.removeAllValues()
-        BlockIDSDK.sharedInstance.resetSDK(licenseKey: Tenant.licenseKey, 
+        BlockIDSDK.sharedInstance.resetSDK(licenseKey: Tenant.licenseKey,
                                            rootTenant: Tenant.defaultTenant,
                                            reason: reason)
     }
