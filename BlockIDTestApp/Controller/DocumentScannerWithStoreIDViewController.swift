1928878fc20a55bfc2e70a6b7ee8d3fee3661639
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2025-12-04 19:15:17 +0530

    MOB-6947-ios-allow-mobile-sdk-to-accept-uid-as-store-id-during-id-proofing-session-creation

---
+++ BlockIDTestApp/Controller/DocumentScannerWithStoreIDViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DocumentScannerWithStoreIDViewController.swift
+//  1Kosmos Demo
+//
+//  Created by Prasanna Gupta on 04/12/25.
+//
+
+import UIKit
+
+class DocumentScannerWithStoreIDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
