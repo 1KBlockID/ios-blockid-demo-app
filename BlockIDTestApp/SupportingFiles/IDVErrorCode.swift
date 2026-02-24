31c2687e4427dc12f948aef78f5ed86b056e4bac
prasannaKosmos <82530183+prasannaKosmos@users.noreply.github.com>
2026-02-24 16:20:10 +0530

    MOB-7032-ios-update-mobile-sdk-to-use-v-3-endpoint-for-document-session-polling

--- BlockIDTestApp/SupportingFiles/IDVErrorCode.swift
+++ BlockIDTestApp/SupportingFiles/IDVErrorCode.swift
@@ -25,36 +25,36 @@ enum IDVErrorCode: String, LocalizedError {
     case documentNotAllowed             = "IDV0015"
     
     private var message: String {
-            switch self {
-                
-            case .imageQualityCheckFailed:
-                return "Scan failed. Please check the surroundings and try scanning again."
-                
-            case .verificationPartiallyCompleted,
-                 .verificationFrontOnly,
-                 .extractionFailed,
-                 .documentLivenessFailed,
-                 .documentValidityFailed,
-                 .fraudCheckFailed:
-                return "We couldn’t complete the verification of the document. Please try again."
-                
-            case .unsupportedDocument,
-                 .documentNotAllowed:
-                return "Unsupported document. Please scan a valid document."
-                
-            case .incorrectDocumentType:
-                return "Scan failed. Please scan a valid document."
-                
-            case .dataCheckFailed,
-                 .moduleFailed:
-                return "We couldn’t complete the verification. Please try again."
-                
-            case .documentExpired:
-                return "The document you are trying to enroll is already expired."
-                
-            case .retryAttemptsExhausted:
-                return "You have exhausted all retry attempts. Please try again later."
-            }
+        switch self {
+            
+        case .imageQualityCheckFailed:
+            return "Scan failed. Please check the surroundings and try scanning again."
+            
+        case .verificationPartiallyCompleted,
+                .verificationFrontOnly,
+                .extractionFailed,
+                .documentLivenessFailed,
+                .documentValidityFailed,
+                .fraudCheckFailed:
+            return "We couldn’t complete the verification of the document. Please try again."
+            
+        case .unsupportedDocument,
+                .documentNotAllowed:
+            return "Unsupported document. Please scan a valid document."
+            
+        case .incorrectDocumentType:
+            return "Scan failed. Please scan a valid document."
+            
+        case .dataCheckFailed,
+                .moduleFailed:
+            return "We couldn’t complete the verification. Please try again."
+            
+        case .documentExpired:
+            return "The document you are trying to enroll is already expired."
+            
+        case .retryAttemptsExhausted:
+            return "You have exhausted all retry attempts. Please try again later."
+        }
     }
     
     var errorDescription: String? {
